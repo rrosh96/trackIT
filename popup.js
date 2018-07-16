ba1bbe72f301e5ef4b30eb99226612a5c90ee27e
RoshanRaju <rroshraju@gmail.com>
2018-07-16 14:35:06 +0530

    TrackIT | Included HTTP request.
    Signed-off-by: RoshanRaju <rroshraju@gmail.com>

--- popup.js
+++ popup.js
@@ -1,11 +1,16 @@
 var angularApp = angular.module('angularApp', []);
+var xhr = new XMLHttpRequest();
+xhr.open("POST", 'http://loc', true);
+
 
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse){
         if(request.msg === "task_trackIT"){
            console.log(request.data.taskName);
-
+           // httpService.postMessage()
+           xhr.send("1234")
         }
     }
 )  
+
