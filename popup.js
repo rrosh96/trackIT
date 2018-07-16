e6a8fee2e6fdfdac8cee5e255e7c29b956512597
RoshanRaju <rroshraju@gmail.com>
2018-07-16 17:31:16 +0530

    TrackIT | Updated Angular Support :
    Signed-off-by: RoshanRaju <rroshraju@gmail.com>

--- popup.js
+++ popup.js
@@ -1,16 +1,34 @@
 var angularApp = angular.module('angularApp', []);
-var xhr = new XMLHttpRequest();
-xhr.open("POST", 'http://loc', true);
-
-
-
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse){
-        if(request.msg === "task_trackIT"){
-           console.log(request.data.taskName);
-           // httpService.postMessage()
-           xhr.send("1234")
-        }
+// var xhr = new XMLHttpRequest();
+// xhr.open("POST", 'http://loc', true);
+angularApp.service('httpService', ["$http", function($http){
+    var self = this;
+    this.postMessage = function(data){
+        $http.post("http://localhost:6007/WebServices/GetNotes", data)
+        .then(function(res){
+            console.log(res);
+        })
+        .catch(function(err){
+            console.log(err);
+        })
     }
-)  
+
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse){
+            if(request.msg === "task_trackIT"){
+               console.log(request.data.taskName);
+               // httpService.postMessage()
+               // xhr.send("1234")
+               self.postMessage({"Notes":request.data.taskName})
+            }
+        }
+    )
+
+}])
+
+angularApp.controller('mainController', ["httpService", function(httpService){
+
+}])
+
+  
 
